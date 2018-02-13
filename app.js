1b4438b1cc20b4851bd0e7d47f240c63108fd7a6
Ajilore Raphael Olamide <raphealolams@gmail.com>
2018-02-13 09:29:54 +0100

    vidjot setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+
+// Middleware for Handlebars
+app.engine('handlebars' , exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine' , 'handlebars');
+
+app.use((req , res, next) => {
+  console.log(Date.now());
+
+  next();
+})
+
+app.get('/' , (req , res) => {
+  res.render('index')
+})
+
+
+const port = 8080;
+app.listen(port , () => {
+  console.log(`App Started On port ${port}`);
+})
