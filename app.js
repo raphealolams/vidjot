22499efd7ec4bcb14ff23a8cd660dbe7b9fe501d
Ajilore Raphael Olamide <raphealolams@gmail.com>
2018-02-21 12:53:32 +0100

    simple crud with create, edit and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -29,13 +30,41 @@ app.set('view engine' , 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// override with POST having ?_method=DELETE
+// middleware
+app.use(methodOverride('_method'))
+
 app.get('/' , (req , res) => {
   res.render('index')
+})
+
+app.get('/ideas' , (req , res) => {
+  Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas => {
+      res.render('ideas/index' , {
+        ideas
+      })
+    })
 })
 
 // Add Ideas
 app.get('/ideas/add' , (req , res) => {
   res.render('ideas/add')
+})
+
+// Edit Idea
+app.get('/ideas/edit/:id' , (req , res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+    .then(idea => {
+      res.render('ideas/edit' , {
+        idea
+      })
+    })
+  
+ 
 })
 
 // Process Form
@@ -66,6 +95,31 @@ app.post('/ideas' , (req , res) => {
         res.redirect('/ideas')
     })
   }
+})
+
+app.put('/ideas/:id' , (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+    .then((idea) => {
+      idea.title = req.body.title;
+      idea.details = req.body.details;
+
+      idea.save()
+        .then(idea => {
+          res.redirect('/ideas')
+        })
+      
+    })
+})
+
+app.delete('/ideas/:id' , (req, res) => {
+  Idea.remove({
+    _id: req.params.id
+  })
+    .then(() => {
+      res.redirect('/ideas')      
+    })
 })
 
 const port = 8080;
